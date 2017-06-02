d3df0475f69dc2ae0023ccd655193a4c6904230c
Aleksandr Slynko <aslynko@connecture.com>
2017-06-02 16:27:46 +0300

    initial commit, working hello world webpack2 + angular2 app

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: {
+    app: "./src/index.ts",
+    polyfill: "./src/polyfill.ts",
+    vendor: "./src/vendor.ts"
+  },
+  output: {
+    filename: "./dest/[name].js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        query: {
+          plugins: ['transform-decorators-legacy']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".ts"]
+  }
+};
